92d29553ba341039f4d48ef077f5610920907a01
Gabriel Schuler Barros <gabrielschuler3@gmail.com>
2021-06-09 16:23:05 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/schuler/CursoSpring/resources/PedidoResource.java
@@ -0,0 +1,24 @@
+package com.schuler.CursoSpring.resources;
+
+import com.schuler.CursoSpring.domain.Categoria;
+import com.schuler.CursoSpring.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias") //end point
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/schuler/CursoSpring/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.schuler.CursoSpring.services;
+
+import com.schuler.CursoSpring.domain.Categoria;
+import com.schuler.CursoSpring.repositories.CategoriaRepository;
+import com.schuler.CursoSpring.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+}
